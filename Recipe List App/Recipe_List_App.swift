5fa74363783b4cd7510cef9707c77a346349c3e8
tbesson <tbesson@mac.com>
2021-03-25 15:07:35 +0900

    Add tab view

--- Recipe List App/Recipe_List_App.swift
+++ Recipe List App/Recipe_List_App.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct Recipe_List_App: App {
     var body: some Scene {
         WindowGroup {
+            // Application starts up calling Recipe Tab View
             RecipeTabView()
         }
     }
